881b9ecd6a02b1bd3d29ecf1862ca5e4f3ec7e36
StukalovSS <mr.luncode@gmail.com>
2019-03-12 21:39:25 +0300

    Add some progs

--- Shteingaus algorithm/HelloWorld.java
+++ Shteingaus algorithm/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
 
     public static void main (String[] args) {
         System.out.println("Hello world");
+        // long x = 38421;
+        // long y = 14092;
         long x = 11108;
         long y = 35218;
         long m = 41903;
@@ -9,20 +11,37 @@ public class HelloWorld {
         long w = 100000000;
 
         System.out.println("Find alpha...");
-        for (long i = 0; i < w; i++) {
-            if (x == modp(pow(q, i), m)){
-                System.out.println("alpha " + i);
-                break;
-            }
+        long alpha = 1;
+        long tmp = modp(pow(q, alpha), m);
+
+        System.out.println(tmp);
+
+        while (modp(tmp, m) != x) {
+            tmp = modp(tmp*q, m);
+            alpha += 1;
         }
+        System.out.println("alpha " + alpha);
 
         System.out.println("Find beta...");
-        for (long i = 0; i < w; i++){
-            if (y == modp(pow(q, i), m)){
-                System.out.println("betta " + i);
-                break;
-            }
+        long beta = 1;
+        tmp = modp(pow(q, beta), m);
+
+        while (modp(tmp, m) != y) {
+            tmp = modp(tmp*q, m);
+            beta += 1;
         }
+        System.out.println("beta " + beta);
+
+        // for (long i = 0; i < w; i++){
+        //     if (y == modp(pow(q, i), m)){
+        //         System.out.println("betta " + i);
+        //         beta = 1;
+        //         break;
+        //     }
+        // }
+
+        System.out.println("SecretKey (alpha): " + modp(pow(y, alpha), m));
+        System.out.println("SecretKey (beta): " + modp(pow(x, beta), m));
         System.out.println("Finish");
     }
 
@@ -36,7 +55,7 @@ public class HelloWorld {
 
     public static long pow(long a, long b) {
         long res = a;
-        for (long i = 0; i < b; i++){
+        for (long i = 0; i < b-1; i++){
             res*=a;
         }
         return res;
